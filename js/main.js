599b38de31e45b08741efddfd9ddb8e3910f30f1
Jaime Garcia Diaz <jgarciadiaz@groupon.com>
2018-06-10 12:42:11 -0500

    feat: make homepage responsive & adjust style

--- js/main.js
+++ js/main.js
@@ -143,24 +143,34 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const content = document.createElement('div');
+  content.className = 'content'
+
+  const info = document.createElement('div');
+  info.className = 'restaurant-info'
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  info.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  info.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  info.append(address);
+
+  content.append(info);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  content.append(more);
 
-  return li
+  li.append(content)
+
+  return li;
 }
 
 /**
